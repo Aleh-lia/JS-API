a76e0e6090ab667cbe2c1b1c0b7ecc9b0775b721
Aleh-lia <liavitski.oleh@yandex.by>
2023-11-01 21:38:58 +0300

    Home Work 3

---
+++ HomeWork/hw3/script.js
@@ -0,0 +1,31 @@
+const ACCESS_KEY = "wrgHFlUcqf0fvl0BvjvT-fuwqybml7CvEzycaZhDCB4";
+
+async function getRandomImage() {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}`
+    );
+    const data = await response.json();
+
+    const image = data.urls.regular;
+    const photographer = data.user.name;
+
+    document.getElementById("image").src = image;
+    document.getElementById("photographer").textContent = photographer;
+  } catch (error) {
+    console.log("Ошибка:", error);
+  }
+}
+
+function likeImage() {
+  const countElement = document.getElementById("count");
+  const count = parseInt(countElement.textContent);
+
+  countElement.textContent = count + 1;
+}
+
+window.onload = function () {
+  getRandomImage();
+};
+
+document.getElementById("likeButton").addEventListener("click", likeImage);
